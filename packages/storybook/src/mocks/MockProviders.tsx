de80828edf4eedee5686aa0711b7d513447a9525
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-01 12:57:58 +0200

    dist/web/mockAuth.js

--- packages/storybook/src/mocks/MockProviders.tsx
+++ packages/storybook/src/mocks/MockProviders.tsx
@@ -5,7 +5,7 @@
 import React from 'react'
 
 import { LocationProvider } from '@cedarjs/router'
-import { useAuth } from '@cedarjs/testing/web'
+import { useAuth } from '@cedarjs/testing/dist/web/mockAuth.js'
 import { RedwoodProvider } from '@cedarjs/web'
 import { RedwoodApolloProvider } from '@cedarjs/web/apollo'
 
