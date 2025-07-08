95a545c0991e76e202fc81b0bd9f847bb48fb2e9
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-08 08:49:45 +0200

    chore(cli): Remove unused babel code in lib/exec

--- packages/cli/src/lib/exec.js
+++ packages/cli/src/lib/exec.js
@@ -1,10 +1,6 @@
 import { createRequire } from 'node:module'
 import path from 'node:path'
 
-import {
-  getWebSideDefaultBabelConfig,
-  registerApiSideBabelHook,
-} from '@cedarjs/babel-config'
 import { getPaths } from '@cedarjs/project-config'
 
 export async function runScriptFunction({
@@ -24,65 +20,4 @@ export async function runScriptFunction({
   }
 
   return returnValue
-}
-
-export async function configureBabel() {
-  const {
-    overrides: _overrides,
-    plugins: webPlugins,
-    ...otherWebConfig
-  } = getWebSideDefaultBabelConfig()
-
-  // Import babel config for running script
-  registerApiSideBabelHook({
-    plugins: [
-      [
-        'babel-plugin-module-resolver',
-        {
-          alias: {
-            $api: getPaths().api.base,
-            $web: getPaths().web.base,
-            api: getPaths().api.base,
-            web: getPaths().web.base,
-          },
-          loglevel: 'silent', // to silence the unnecessary warnings
-        },
-        'exec-$side-module-resolver',
-      ],
-    ],
-    overrides: [
-      {
-        test: ['./api/'],
-        plugins: [
-          [
-            'babel-plugin-module-resolver',
-            {
-              alias: {
-                src: getPaths().api.src,
-              },
-              loglevel: 'silent',
-            },
-            'exec-api-src-module-resolver',
-          ],
-        ],
-      },
-      {
-        test: ['./web/'],
-        plugins: [
-          ...webPlugins,
-          [
-            'babel-plugin-module-resolver',
-            {
-              alias: {
-                src: getPaths().web.src,
-              },
-              loglevel: 'silent',
-            },
-            'exec-web-src-module-resolver',
-          ],
-        ],
-        ...otherWebConfig,
-      },
-    ],
-  })
 }
