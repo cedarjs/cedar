c64944df303360b567b010cc9496e6ca57eb2a27
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-09 09:33:10 +0200

    Remove execBabel

--- packages/cli/src/commands/prerenderHandler.js
+++ packages/cli/src/commands/prerenderHandler.js
@@ -10,7 +10,7 @@ import { getConfig, getPaths } from '@cedarjs/project-config'
 import { errorTelemetry } from '@cedarjs/telemetry'
 
 import c from '../lib/colors.js'
-import { configureBabel, runScriptFunction } from '../lib/execBabel.js'
+import { runScriptFunction } from '../lib/exec.js'
 
 class PathParamError extends Error {}
 
@@ -139,8 +139,6 @@ export const getTasks = async (dryrun, routerPathFilter = null) => {
     process.exit(1)
     // TODO: Run this automatically at this point.
   }
-
-  configureBabel()
 
   const expandedRouteParameters = await Promise.all(
     prerenderRoutes.map((route) => expandRouteParameters(route)),
