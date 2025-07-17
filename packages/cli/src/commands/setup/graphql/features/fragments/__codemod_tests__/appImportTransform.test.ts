b830b0d6f2f5a5fe95df1dfb30a475e355997780
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-17 18:20:37 +0200

    Remove setImmediate workaround

--- packages/cli/src/commands/setup/graphql/features/fragments/__codemod_tests__/appImportTransform.test.ts
+++ packages/cli/src/commands/setup/graphql/features/fragments/__codemod_tests__/appImportTransform.test.ts
@@ -1,20 +1,14 @@
-import { afterEach, describe, test } from 'vitest'
+import { describe, test } from 'vitest'
 
 describe('fragments possibleTypes import', () => {
-  afterEach(async () => {
-    // The fast implementation should eliminate the need for this workaround
-    // but keeping minimal cleanup just in case
-    await new Promise((res) => setImmediate(res))
-  })
-
   test('Default App.tsx', async () => {
-    await matchFolderTransformFast('appImportTransform', 'import-simple', {
+    await matchFolderTransform('appImportTransform', 'import-simple', {
       useJsCodeshift: true,
     })
   })
 
   test('App.tsx with existing import', async () => {
-    await matchFolderTransformFast('appImportTransform', 'existingImport', {
+    await matchFolderTransform('appImportTransform', 'existingImport', {
       useJsCodeshift: true,
     })
   })
