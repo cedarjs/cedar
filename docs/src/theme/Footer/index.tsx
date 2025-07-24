9bd7c05edfd902646b30d8aa349fb9a95fefec34
Tobbe Lundberg <tobbe@tlundberg.com>
2025-07-24 11:49:00 +0200

    swizzle eject

--- docs/src/theme/Footer/index.tsx
+++ docs/src/theme/Footer/index.tsx
@@ -1,24 +1,26 @@
 import React, { type ReactNode } from 'react'
 
-import type { WrapperProps } from '@docusaurus/types'
-import type FooterType from '@theme/Footer'
-import Footer from '@theme-original/Footer'
+import { useThemeConfig } from '@docusaurus/theme-common'
+import FooterCopyright from '@theme/Footer/Copyright'
+import FooterLayout from '@theme/Footer/Layout'
+import FooterLinks from '@theme/Footer/Links'
+import FooterLogo from '@theme/Footer/Logo'
 
-type Props = WrapperProps<typeof FooterType>
+function Footer(): ReactNode {
+  const { footer } = useThemeConfig()
+  if (!footer) {
+    return null
+  }
+  const { copyright, links, logo, style } = footer
 
-export default function FooterWrapper(props: Props): ReactNode {
   return (
-    <>
-      <div>
-        <p>Built with Docusaurus. Hosted by Netlify.</p>
-        <a href="https://www.netlify.com">
-          <img
-            src="https://www.netlify.com/assets/badges/netlify-badge-light.svg"
-            alt="Deploys by Netlify"
-          />
-        </a>
-      </div>
-      <Footer {...props} />
-    </>
+    <FooterLayout
+      style={style}
+      links={links && links.length > 0 && <FooterLinks links={links} />}
+      logo={logo && <FooterLogo logo={logo} />}
+      copyright={copyright && <FooterCopyright copyright={copyright} />}
+    />
   )
 }
+
+export default React.memo(Footer)
