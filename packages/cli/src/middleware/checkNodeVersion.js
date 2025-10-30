bd75d9fa104be1e682066c15b24b8db6bd45b34e
Tobbe Lundberg <tobbe@tlundberg.com>
2025-10-30 15:37:07 +0100

    Restore unrelated changes

--- packages/cli/src/middleware/checkNodeVersion.js
+++ packages/cli/src/middleware/checkNodeVersion.js
@@ -7,18 +7,19 @@ export function checkNodeVersion() {
 
   const pVersion = process.version
   const pVersionC = semver.clean(pVersion)
-  const LOWER_BOUND = 'v24.0.0'
+  const LOWER_BOUND = 'v20.0.0'
+  const LOWER_BOUND_C = semver.clean(LOWER_BOUND)
 
-  if (semver.gt(pVersionC, semver.clean(LOWER_BOUND))) {
+  if (semver.gt(pVersionC, LOWER_BOUND_C)) {
     return checks
   }
 
   checks.ok = false
   checks.message = [
-    `Your Node.js version is ${c.warning(pVersion)}, but Cedar requires ` +
-      `${c.important(`>=${LOWER_BOUND}`)}.`,
-    'Upgrade your Node.js version using `nvm`, `fnm` or a similar tool. See ' +
-      'https://cedarjs.com/docs/how-to/using-nvm.',
+    `Your Node.js version is ${c.warning(
+      pVersion,
+    )}, but Redwood requires ${c.important(`>=${LOWER_BOUND}`)}.`,
+    'Upgrade your Node.js version using `nvm` or a similar tool. See https://redwoodjs.com/docs/how-to/using-nvm.',
   ].join('\n')
 
   return checks
