afa34942ca32037561f15a64ed3530ae7e38f213
Tobbe Lundberg <tobbe@tlundberg.com>
2025-10-30 15:51:42 +0100

    checkNodeVersion

--- packages/cli/src/middleware/checkNodeVersion.js
+++ packages/cli/src/middleware/checkNodeVersion.js
@@ -7,19 +7,18 @@ export function checkNodeVersion() {
 
   const pVersion = process.version
   const pVersionC = semver.clean(pVersion)
-  const LOWER_BOUND = 'v20.0.0'
-  const LOWER_BOUND_C = semver.clean(LOWER_BOUND)
+  const LOWER_BOUND = 'v24.0.0'
 
-  if (semver.gt(pVersionC, LOWER_BOUND_C)) {
+  if (semver.gte(pVersionC, LOWER_BOUND)) {
     return checks
   }
 
   checks.ok = false
   checks.message = [
-    `Your Node.js version is ${c.warning(
-      pVersion,
-    )}, but Redwood requires ${c.important(`>=${LOWER_BOUND}`)}.`,
-    'Upgrade your Node.js version using `nvm` or a similar tool. See https://redwoodjs.com/docs/how-to/using-nvm.',
+    `Your Node.js version is ${c.warning(pVersion)}, but Cedar requires ` +
+      `${c.important(`>= ${LOWER_BOUND}`)}.`,
+    'Upgrade your Node.js version using `nvm`, `n`, or a similar tool. See ' +
+      'https://cedarjs.com/docs/how-to/using-nvm.',
   ].join('\n')
 
   return checks
