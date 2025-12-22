d02b920387e0ff23cd2f47bf78b0c6b918915cd5
Tobbe Lundberg <tobbe@tlundberg.com>
2025-12-22 09:12:38 +0100

    update tests

--- packages/cli/src/commands/generate/package/__tests__/package.test.ts
+++ packages/cli/src/commands/generate/package/__tests__/package.test.ts
@@ -6,6 +6,7 @@ import path from 'path'
 
 import { describe, it, expect } from 'vitest'
 
+// @ts-expect-error - Importing a JS file
 import * as packageHandler from '../packageHandler.js'
 
 describe('packageHandler', () => {
@@ -26,11 +27,12 @@ describe('packageHandler', () => {
   })
 
   describe('files', () => {
-    describe('Single word default files', async () => {
+    const packagesPath = '/path/to/project/api/src/packages'
+
+    describe('single word package names', async () => {
       const files = await packageHandler.files({ name: '@my-org/foo' })
 
-      it.only('creates a single word package', () => {
-        const packagesPath = '/path/to/project/api/src/packages'
+      it('creates a single word package', () => {
         expect(
           files[path.normalize(packagesPath + '/foo/src/index.ts')],
         ).toMatchSnapshot('Package index')
@@ -49,42 +51,68 @@ describe('packageHandler', () => {
       })
     })
 
-    describe('multi-word files', () => {
-      it('creates a multi word function file', async () => {
-        const multiWordDefaultFiles = await packageHandler.files({
-          name: 'send-mail',
-          queueName: 'default',
-          tests: false,
-          typescript: true,
+    // I had to decide if I wanted the folder name for multi-word packages to be
+    // hyphenated (kebab-case) or camelCase. I decided to use kebab-case because
+    // it matches what the package name is. So, just like we use PascalCase for
+    // folder names for React components, we use kebab-case for folder names for
+    // packages.
+    describe('multi-word package names', () => {
+      it('creates a multi-word package', async () => {
+        const files = await packageHandler.files({
+          name: '@my-org/form-validators',
         })
 
-        expect(
-          multiWordDefaultFiles[
-            path.normalize(
-              '/path/to/project/api/src/functions/SendMailJob/SendMailJob.js',
-            )
-          ],
-        ).toMatchSnapshot()
+        const indexPath = path.normalize(
+          packagesPath + '/form-validators/src/index.ts',
+        )
+        const testPath = path.normalize(
+          packagesPath + '/form-validators/src/formValidators.test.ts',
+        )
+        const scenarioPath = path.normalize(
+          packagesPath + '/form-validators/src/formValidators.scenarios.ts',
+        )
+
+        expect(files[indexPath]).toMatchSnapshot('Package index')
+        expect(files[testPath]).toMatchSnapshot('Test snapshot')
+        expect(files[scenarioPath]).toMatchSnapshot('Scenario snapshot')
+      })
+
+      it('creates a multiWord package', async () => {
+        const files = await packageHandler.files({
+          name: '@my-org/formValidators',
+        })
+
+        const indexPath = path.normalize(
+          packagesPath + '/form-validators/src/index.ts',
+        )
+        const testPath = path.normalize(
+          packagesPath + '/form-validators/src/formValidators.test.ts',
+        )
+        const scenarioPath = path.normalize(
+          packagesPath + '/form-validators/src/formValidators.scenarios.ts',
+        )
+
+        expect(files[indexPath]).toMatchSnapshot('Package index')
+        expect(files[testPath]).toMatchSnapshot('Test snapshot')
+        expect(files[scenarioPath]).toMatchSnapshot('Scenario snapshot')
       })
     })
 
     describe('generation of js files', async () => {
       const jsFiles = await packageHandler.files({
         name: 'Sample',
-        queueName: 'default',
-        tests: true,
         typescript: false,
       })
 
-      it('returns tests, scenario and job file for JS', () => {
+      it('returns tests, scenario and main package file for JS', () => {
         const fileNames = Object.keys(jsFiles)
         expect(fileNames.length).toEqual(3)
 
         expect(fileNames).toEqual(
           expect.arrayContaining([
-            expect.stringContaining('SampleJob.js'),
-            expect.stringContaining('SampleJob.test.js'),
-            expect.stringContaining('SampleJob.scenarios.js'),
+            expect.stringContaining('index.js'),
+            expect.stringContaining('sample.test.js'),
+            expect.stringContaining('sample.scenarios.js'),
           ]),
         )
       })
