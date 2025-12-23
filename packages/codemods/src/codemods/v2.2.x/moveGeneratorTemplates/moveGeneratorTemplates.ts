c659096c2a3582151de0fec873e6a4404d41dbd5
Tobbe Lundberg <tobbe@tlundberg.com>
2025-12-23 06:37:57 +0100

    tweak codemod

--- packages/codemods/src/codemods/v2.2.x/moveGeneratorTemplates/moveGeneratorTemplates.ts
+++ packages/codemods/src/codemods/v2.2.x/moveGeneratorTemplates/moveGeneratorTemplates.ts
@@ -1,18 +1,17 @@
-import fs from 'fs'
-import path from 'path'
+import fs from 'node:fs'
+import path from 'node:path'
 
 import { getPaths } from '@cedarjs/project-config'
 
 async function moveGeneratorTemplates() {
-  const apiGeneratorsOld = path.join(getPaths().base, 'api', 'generators')
-  const webGeneratorsOld = path.join(getPaths().base, 'web', 'generators')
+  const apiGeneratorsOld = path.join(getPaths().api.base, 'generators')
+  const webGeneratorsOld = path.join(getPaths().web.base, 'generators')
 
   const generatorsDir = path.join(getPaths().base, 'generatorTemplates')
 
   const move = (oldPath: string, newPath: string) => {
     if (fs.existsSync(oldPath)) {
       fs.mkdirSync(path.dirname(newPath), { recursive: true })
-      // We use copy and remove to be safe across partitions and to act recursively
       fs.cpSync(oldPath, newPath, { recursive: true })
       fs.rmSync(oldPath, { recursive: true, force: true })
     }
