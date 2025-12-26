2868386a4528bd0329b03c63307a64b57f76b980
Tobbe Lundberg <tobbe@tlundberg.com>
2025-12-26 11:28:53 +0100

    chore(deps): Remove dependency tempy (#846)

--- packages/cli/src/testUtils/index.ts
+++ packages/cli/src/testUtils/index.ts
@@ -1,9 +1,5 @@
-import fs from 'fs'
-import path from 'path'
-
 import { format } from 'prettier'
 import parserBabel from 'prettier/parser-babel'
-import tempy from 'tempy'
 
 export const formatCode = async (code: string) => {
   return format(code, {
@@ -12,12 +8,4 @@ export const formatCode = async (code: string) => {
     // We have it because babel-plugin-tester pulls it in
     plugins: [parserBabel],
   })
-}
-
-export const createProjectMock = () => {
-  const tempDir = tempy.directory()
-  // add fake redwood.toml
-  fs.closeSync(fs.openSync(path.join(tempDir, 'redwood.toml'), 'w'))
-
-  return tempDir
 }
