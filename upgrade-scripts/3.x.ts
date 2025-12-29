66c457c304c91b3eb39c9c9425a8e0628c5acffd
Tobbe Lundberg <tobbe@tlundberg.com>
2025-12-29 19:36:53 +0100

    feat(upgrade-scripts): 3.x upgrade script and updated canary script
    Adding a package.json file to make it easier to test the scripts locally wihtout Node complaining about ESM syntax

--- upgrade-scripts/3.x.ts
+++ upgrade-scripts/3.x.ts
@@ -3,19 +3,6 @@ import path from 'node:path'
 
 import { getPaths } from '@cedarjs/project-config'
 
-const rootPackageJsonPath = path.join(getPaths().base, 'package.json')
-const packageJson = JSON.parse(
-  await fs.promises.readFile(rootPackageJsonPath, 'utf8'),
-)
-
-if (!Array.isArray(packageJson.workspaces)) {
-  console.log('Deprecated workspace config detected in ' + rootPackageJsonPath)
-  console.log(
-    'Please see https://github.com/cedarjs/cedar/releases/tag/v2.2.0 for ' +
-      'more information.',
-  )
-}
-
 const apiGeneratorTemplatesPath = path.join(getPaths().api.base, 'generators')
 const webGeneratorTemplatesPath = path.join(getPaths().web.base, 'generators')
 
