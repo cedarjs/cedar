80122067358628bee6c9904b3cb378509f41ec9e
Tobbe Lundberg <tobbe@tlundberg.com>
2026-01-03 06:12:46 +0100

    feat(cli): `cedar build` now also builds all packages (#915)

--- packages/cli/src/lib/project.js
+++ packages/cli/src/lib/project.js
@@ -11,17 +11,38 @@ export const isTypeScriptProject = () => {
   )
 }
 
-export const workspaces = () => {
-  const paths = getPaths()
+export function workspaces({ includePackages = false } = {}) {
+  const cedarPaths = getPaths()
 
   let workspaces = []
 
-  if (fs.existsSync(path.join(paths.web.base, 'package.json'))) {
+  if (fs.existsSync(path.join(cedarPaths.web.base, 'package.json'))) {
     workspaces = [...workspaces, 'web']
   }
 
-  if (fs.existsSync(path.join(paths.api.base, 'package.json'))) {
+  if (fs.existsSync(path.join(cedarPaths.api.base, 'package.json'))) {
     workspaces = [...workspaces, 'api']
+  }
+
+  if (includePackages) {
+    // fs.globSync requires forward slashes as path separators in patterns,
+    // even on Windows.
+    const globPattern = path
+      .join(cedarPaths.packages, '*')
+      .replaceAll('\\', '/')
+    // TODO: See if we can make this async
+    const allPackagePaths = fs.globSync(globPattern)
+
+    workspaces = [
+      ...workspaces,
+      'packages/*',
+      ...allPackagePaths.map((p) => p.split('/').at(-1)),
+      ...allPackagePaths.map((p) => p.split('/').slice(-2).join('/')),
+      ...allPackagePaths.map((p) => {
+        const packageJsonPath = path.join(p, 'package.json')
+        return JSON.parse(fs.readFileSync(packageJsonPath, 'utf8')).name
+      }),
+    ]
   }
 
   return workspaces
