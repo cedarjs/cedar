d3aad18d1f09d16798e68f65791cbaec33da9ee8
Tobbe Lundberg <tobbe@tlundberg.com>
2026-01-06 23:33:07 +0100

    Also update test fixtures

--- __fixtures__/esm-test-project/web/src/pages/WaterfallPage/WaterfallPage.test.tsx
+++ __fixtures__/esm-test-project/web/src/pages/WaterfallPage/WaterfallPage.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@cedarjs/testing/web'
 
 import WaterfallPage from './WaterfallPage'
 
-//   Improve this test with help from the Redwood Testing Doc:
+//   Improve this test with help from the CedarJS Testing Doc:
 //   https://cedarjs.com/docs/testing#testing-pages-layouts
 
 describe('WaterfallPage', () => {
--- __fixtures__/test-project/web/src/pages/DoublePage/DoublePage.test.tsx
+++ __fixtures__/test-project/web/src/pages/DoublePage/DoublePage.test.tsx
@@ -2,7 +2,7 @@ import { render } from '@cedarjs/testing/web'
 
 import DoublePage from './DoublePage'
 
-//   Improve this test with help from the Redwood Testing Doc:
+//   Improve this test with help from the CedarJS Testing Doc:
 //   https://cedarjs.com/docs/testing#testing-pages-layouts
 
 describe('DoublePage', () => {
