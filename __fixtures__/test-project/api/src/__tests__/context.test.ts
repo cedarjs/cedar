9dd027a959b9206ad2b11c1ee09785c2f38e9c83
Tobbe Lundberg <tobbe@tlundberg.com>
2026-01-11 06:58:49 +0100

    0o7

--- __fixtures__/test-project/api/src/__tests__/context.test.ts
+++ __fixtures__/test-project/api/src/__tests__/context.test.ts
@@ -1,6 +1,6 @@
 test('Set a mock user on the context', async () => {
   const user = {
-    id: '4c3d3e8e-2b1a-4f5c-8c7d-9e0f1a2b3c4d',
+    id: '00700000-0070-4007-A007-007000000000',
     name: 'Bond, James Bond',
     email: 'totallyNotASpy@example.com',
     roles: 'secret_agent',
