e5edea0573fae97c9597378eca3dc604385f8e25
Tobbe Lundberg <tobbe@tlundberg.com>
2026-01-16 14:48:05 +0100

    chore(ci): Skip full CI for some .github/ files

--- .github/actions/detect-changes/cases/code_changes.mjs
+++ .github/actions/detect-changes/cases/code_changes.mjs
@@ -29,12 +29,44 @@ export function isChangesetsFile(filePath) {
   return /^\.changesets\/.*\.md/.test(filePath)
 }
 
+function isNonCodeWorkflowOrAction(filePath) {
+  const nonCodeWorkflowsOrActions = [
+    '.github/workflows/check-changelog.yml',
+    '.github/actions/check_changesets/check_changesets.mjs',
+    '.github/actions/check_changesets/action.yml',
+    '.github/actions/check_changesets/package.json',
+    '.github/actions/check_changesets/yarn.lock',
+
+    '.github/workflows/publish-canary.yml',
+    '.github/scripts/publish_canary.sh',
+
+    '.github/workflows/publish-release-candidate.yml',
+    '.github/scripts/publish-release-candidate.ts',
+
+    '.github/workflows/require-milestone.yml',
+    '.github/actions/action.yml',
+    '.github/actions/requireMilestone.mjs',
+
+    '.github/workflows/require-release-label.yml',
+    '.github/actions/require-release-label-or-cc-message/action.yml',
+    '.github/actions/require-release-label-or-cc-message/requireReleaseLabelOrCcMessage.mts',
+
+    '.github/workflows/scorecard.yml',
+    '.github/workflows/stale.yml',
+  ]
+  return nonCodeWorkflowsOrActions.includes(filePath)
+}
+
 /**
  * Checks if the given array of file paths contains any framework code files
  */
 export function codeChanges(changedFiles) {
   return changedFiles.some((file) => {
-    if (!isDocsFile(file) && !isChangesetsFile(file)) {
+    if (
+      !isDocsFile(file) &&
+      !isChangesetsFile(file) &&
+      !isNonCodeWorkflowOrAction(file)
+    ) {
       console.log(`Found potential code file: ${file}`)
       return true
     }
