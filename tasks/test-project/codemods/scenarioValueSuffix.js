2c71e5c153f373e303e15f4c4aac61a35df27bff
Tobbe Lundberg <tobbe@tlundberg.com>
2026-01-27 18:59:27 +0100

    unrandomize strings

--- tasks/test-project/codemods/scenarioValueSuffix.js
+++ tasks/test-project/codemods/scenarioValueSuffix.js
@@ -1,4 +1,5 @@
 const stringWithSuffixRegex = /String\d+$/
+const emailWithRandomNumberRegex = /foo\d+@bar\.com$/
 
 export default (file, api) => {
   const j = api.jscodeshift
@@ -11,6 +12,8 @@ export default (file, api) => {
       const stringValue = obj.value.value
       if (stringWithSuffixRegex.test(stringValue)) {
         obj.value.value = `String${obj.value.loc.start.line}`
+      } else if (emailWithRandomNumberRegex.test(stringValue)) {
+        obj.value.value = `foo${obj.value.loc.start.line}@bar.com`
       }
     })
     .toSource()
