6a0bc0babc119d3996060854b2ce7b4695797524
Tobbe Lundberg <tobbe@tlundberg.com>
2026-02-20 15:05:05 -0600

    chore(cli): More internal renaming to Cedar

--- packages/cli/src/middleware/detectProjectCedarVersion.ts
+++ packages/cli/src/middleware/detectProjectCedarVersion.ts
@@ -1,9 +1,9 @@
 // @ts-expect-error - Types not available for JS files
 import { getInstalledCedarVersion } from '../lib/index.js'
 
-type RxVersionArgv = Record<string, unknown> & { rwVersion?: string }
+type CedarVersionArgv = Record<string, unknown> & { rwVersion?: string }
 
-export default async function detectRxVersion(argv: RxVersionArgv) {
+export async function detectCedarVersion(argv: CedarVersionArgv) {
   if (!argv.rwVersion) {
     return {
       rwVersion: await getInstalledCedarVersion(),
