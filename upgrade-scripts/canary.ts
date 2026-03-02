f0dabc9f736b7dd1c04984c71569d29ebe4f2a66
Tobbe Lundberg <tobbe@tlundberg.com>
2026-03-02 02:21:45 -0600

    feat(upgrade-scripts): 2.7.x (#1289)

--- upgrade-scripts/canary.ts
+++ upgrade-scripts/canary.ts
@@ -40,3 +40,42 @@ if (fs.existsSync(apiGeneratorTemplatesPath)) {
       'information.',
   )
 }
+
+const dbTsPath = path.join(getPaths().api.lib, 'db.ts')
+const dbJsPath = path.join(getPaths().api.lib, 'db.js')
+
+const dbFilePath = fs.existsSync(dbTsPath)
+  ? dbTsPath
+  : fs.existsSync(dbJsPath)
+    ? dbJsPath
+    : null
+
+const hasPrismaV7PrepExport =
+  dbFilePath &&
+  /export\s+\*\s+from\s+['"]@prisma\/client['"]/.test(
+    fs.readFileSync(dbFilePath, 'utf8'),
+  )
+
+if (!hasPrismaV7PrepExport) {
+  console.log(
+    'After completing the upgrade to this Canary build of CedarJS we\n' +
+      'recommend that you run the new Prisma v7 preparation codemod.\n',
+  )
+  console.log(
+    'As the name implies, the codemod will prepare your codebase for the\n' +
+      'Prisma v7 upgrade that will come in the next major version of\n' +
+      'CedarJS. The codemod will add a new export of the Prisma client, and\n' +
+      'updates all existing `@prisma/client` imports to use this new export.\n',
+  )
+  console.log(
+    'The codemod is not required – you can keep doing the direct\n' +
+      '`@prisma/client` import everywhere for now, but for Prisma v7 that\n' +
+      'will not work anymore. We recommend doing the code change now, to\n' +
+      'make the upgrade to the next major version of Cedar (and Prisma)\n' +
+      'easier and lower risk.\n',
+  )
+  console.log(
+    'Please run the following command after the upgrade has completed:',
+  )
+  console.log('  yarn dlx @cedarjs/codemods prisma-v7-prep')
+}
