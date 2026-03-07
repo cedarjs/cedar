b083d45b6657d330b400103c843c4cc9bd0d6fc9
Tobbe Lundberg <tobbe@tlundberg.com>
2026-03-07 16:52:52 +0100

    Update templates

--- packages/create-cedar-app/templates/js/api/src/lib/db.js
+++ packages/create-cedar-app/templates/js/api/src/lib/db.js
@@ -1,16 +1,19 @@
 // See https://www.prisma.io/docs/reference/tools-and-interfaces/prisma-client/constructor
 // for options.
 
-import { PrismaClient } from '@prisma/client'
+import { PrismaBetterSqlite3 } from '@prisma/adapter-better-sqlite3'
+import { PrismaClient } from 'api/generated/prisma'
 
 import { emitLogLevels, handlePrismaLogging } from '@cedarjs/api/logger'
 
 import { logger } from './logger.js'
 
-export * from '@prisma/client'
+export * from 'api/generated/prisma'
 
+const adapter = new PrismaBetterSqlite3({ url: process.env.DATABASE_URL })
 const prismaClient = new PrismaClient({
   log: emitLogLevels(['info', 'warn', 'error']),
+  adapter,
 })
 
 handlePrismaLogging({
