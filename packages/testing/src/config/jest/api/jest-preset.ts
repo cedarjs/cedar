1da1bb309528183de40f8a13a6a9b8169b2cb118
Tobbe Lundberg <tobbe@tlundberg.com>
2026-03-09 01:31:18 +0100

    Fix jest preset

--- packages/testing/src/config/jest/api/jest-preset.ts
+++ packages/testing/src/config/jest/api/jest-preset.ts
@@ -31,7 +31,7 @@ const config: Config = {
     name: 'api',
   },
   collectCoverageFrom: [
-    '**/*.{js,jsx,ts,tsx}',
+    '**/*.{js,jsx,ts,tsx,mts,cts}',
     '!**/node_modules/**',
     '!**/dist/**',
   ],
@@ -45,6 +45,8 @@ const config: Config = {
   // Note this setup runs for each test file!
   setupFilesAfterEnv: [path.join(__dirname, './jest.setup.js')],
   moduleNameMapper: {
+    '^api/(.*)$': path.join(rwjsPaths.base, 'api/$1'),
+    '^src/(.*)$': path.join(rwjsPaths.api.src, '$1'),
     // @NOTE: Import @cedarjs/testing in api tests, and it automatically remaps to the api side only
     // This is to prevent web stuff leaking into api, and vice versa
     '^@cedarjs/testing$': path.join(NODE_MODULES_PATH, '@cedarjs/testing/api'),
@@ -52,7 +54,7 @@ const config: Config = {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
-    '\\.[jt]sx?$': [
+    '\\.[cm]?[jt]sx?$': [
       'babel-jest',
       // When jest runs tests in parallel, it serializes the config before passing down options to babel
       // that's why these must be serializable. So ideally, we should just pass reference to a
