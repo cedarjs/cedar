redwoodjs/redwood

fix(exec): only pass script args to script (#11957)

packages/cli/src/commands/__tests__/exec.test.ts
@@ -1,14 +1,27 @@
 import path from 'node:path'
 
+import { fs as memfs, vol } from 'memfs'
 import { vi, afterEach, beforeEach, describe, it, expect } from 'vitest'
 
+import { runScriptFunction } from '../../lib/exec'
 import '../../lib/mockTelemetry'
 import { handler } from '../execHandler'
 
+vi.mock('@redwoodjs/babel-config', () => ({
+  getWebSideDefaultBabelConfig: () => ({
+    presets: [],
+    plugins: [],
+  }),
+  registerApiSideBabelHook: () => {},
+}))
+
 vi.mock('@redwoodjs/project-config', () => ({
   getPaths: () => ({
+    api: { base: '', src: '' },
+    web: { base: '', src: '' },
     scripts: path.join('redwood-app', 'scripts'),
   }),
+  getConfig: () => ({}),
   resolveFile: (path: string) => path,
 }))
 
@@ -26,12 +39,55 @@ vi.mock('@redwoodjs/internal/dist/files', () => ({
   },
 }))
 
+vi.mock('../../lib/exec', () => ({
+  runScriptFunction: vi.fn(),
+}))
+
+vi.mock('fs', () => ({ ...memfs, default: { ...memfs } }))
+vi.mock('node:fs', () => ({ ...memfs, default: { ...memfs } }))
+
 beforeEach(() => {
   vi.spyOn(console, 'log').mockImplementation(() => {})
 })
 
 afterEach(() => {
   vi.mocked(console).log.mockRestore()
+})
+
+describe('yarn rw exec', () => {
+  it('passes args on to the script', async () => {
+    vol.fromJSON({
+      'redwood.toml': '# redwood.toml',
+      [path.join('redwood-app', 'scripts', 'normalScript.ts')]: '// script',
+    })
+
+    // Running:
+    // `yarn rw exec normalScript positional1 --no-prisma positional2 --arg1=foo --arg2 bar`
+    const args = {
+      _: ['exec', 'positional1', 'positional2'],
+      prisma: false,
+      arg1: 'foo',
+      arg2: 'bar',
+      list: false,
+      l: false,
+      silent: false,
+      s: false,
+      $0: 'rw',
+      name: 'normalScript',
+    }
+    await handler(args)
+    expect(runScriptFunction).toHaveBeenCalledWith({
+      args: {
+        args: {
+          _: ['positional1', 'positional2'],
+          arg1: 'foo',
+          arg2: 'bar',
+        },
+      },
+      functionName: 'default',
+      path: path.join('redwood-app', 'scripts', 'normalScript.ts'),
+    })
+  })
 })
 
 describe('yarn rw exec --list', () => {

cedarjs/cedar

packages/cli/src/commands/__tests__/exec.test.ts
@@ -1,14 +1,28 @@
 import path from 'node:path'
 
+import { fs as memfs, vol } from 'memfs'
 import { vi, afterEach, beforeEach, describe, it, expect } from 'vitest'
 
+// @ts-expect-error - No types for JS files
+import { runScriptFunction } from '../../lib/exec.js'
 import '../../lib/mockTelemetry.js'
 import { handler } from '../execHandler.js'
 
-vi.mock('@cedarjs/project-config', () => ({
-  getPaths: () => ({
-    scripts: path.join('cedar-app', 'scripts'),
+vi.mock('@redwoodjs/babel-config', () => ({
+  getWebSideDefaultBabelConfig: () => ({
+    presets: [],
+    plugins: [],
   }),
+  registerApiSideBabelHook: () => {},
+}))
+
+vi.mock('@redwoodjs/project-config', () => ({
+  getPaths: () => ({
+    api: { base: '', src: '' },
+    web: { base: '', src: '' },
+    scripts: path.join('redwood-app', 'scripts'),
+  }),
+  getConfig: () => ({}),
   resolveFile: (path: string) => path,
 }))
 
@@ -26,12 +40,55 @@ vi.mock('@cedarjs/internal/dist/files', () => ({
   },
 }))
 
+vi.mock('../../lib/exec', () => ({
+  runScriptFunction: vi.fn(),
+}))
+
+vi.mock('fs', () => ({ ...memfs, default: { ...memfs } }))
+vi.mock('node:fs', () => ({ ...memfs, default: { ...memfs } }))
+
 beforeEach(() => {
   vi.spyOn(console, 'log').mockImplementation(() => {})
 })
 
 afterEach(() => {
   vi.mocked(console).log.mockRestore()
+})
+
+describe('yarn cedar exec', () => {
+  it('passes args on to the script', async () => {
+    vol.fromJSON({
+      'redwood.toml': '# redwood.toml',
+      [path.join('redwood-app', 'scripts', 'normalScript.ts')]: '// script',
+    })
+
+    // Running:
+    // `yarn rw exec normalScript positional1 --no-prisma positional2 --arg1=foo --arg2 bar`
+    const args = {
+      _: ['exec', 'positional1', 'positional2'],
+      prisma: false,
+      arg1: 'foo',
+      arg2: 'bar',
+      list: false,
+      l: false,
+      silent: false,
+      s: false,
+      $0: 'rw',
+      name: 'normalScript',
+    }
+    await handler(args)
+    expect(runScriptFunction).toHaveBeenCalledWith({
+      args: {
+        args: {
+          _: ['positional1', 'positional2'],
+          arg1: 'foo',
+          arg2: 'bar',
+        },
+      },
+      functionName: 'default',
+      path: path.join('redwood-app', 'scripts', 'normalScript.ts'),
+    })
+  })
 })
 
 describe('yarn cedar exec --list', () => {