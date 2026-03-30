74af85baf3d09df5a096d7ff792f39303f3cfbd6
Tobbe Lundberg <tobbe@tlundberg.com>
2026-03-30 07:04:08 +0200

    db.ts parse DATABASE_URL

--- packages/create-cedar-app/database-overlays/pglite/api/src/lib/db.ts
+++ packages/create-cedar-app/database-overlays/pglite/api/src/lib/db.ts
@@ -12,18 +12,19 @@ import { logger } from './logger.js'
 
 export * from 'api/db/generated/prisma/client.mts'
 
+const url = new URL(process.env.DATABASE_URL)
 const pgDataDir = path.join(getPaths().api.base, 'db', 'pglite-data')
+
 const pglite = await PGlite.create(pgDataDir)
 const pgliteServer = new PGLiteSocketServer({
   db: pglite,
-  port: 5433,
-  host: '127.0.0.1',
+  port: parseInt(url.port, 10),
+  host: url.hostname,
 })
+
 await pgliteServer.start()
 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL,
-})
+const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL })
 const prismaClient = new PrismaClient({
   log: emitLogLevels(['info', 'warn', 'error']),
   adapter,
