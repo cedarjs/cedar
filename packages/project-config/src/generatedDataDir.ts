12e2f772c2ab6379e5c2c86d87c06d275625073c
Tobbe Lundberg <tobbe@tlundberg.com>
2026-04-01 12:12:26 +0200

    no findUp

--- packages/project-config/src/generatedDataDir.ts
+++ packages/project-config/src/generatedDataDir.ts
@@ -1,25 +1,29 @@
+import fs from 'node:fs'
 import path from 'node:path'
 
 import { getConfigPath } from './configPath.js'
-import { findUp } from './findUp.js'
 
 /**
- * Search the parent directories for the directory Cedar stores generated and
- * other transatory data in.
+ * Based on the location of the project config file (e.g. cedar.toml), it checks
+ * for the existence of `.cedar/` or `.redwood/` directories. If either is found,
+ * it returns the path to that directory.
  *
- * If neither `.cedar` nor `.redwood` exists yet it default to `.cedar` in the
- * resolved project root.
+ * If neither directory exists yet it default to `.cedar/` in the resolved
+ * project root.
  */
 export function getGeneratedDataDirPath(
   cwd: string = process.env.CEDAR_CWD ?? process.env.RWJS_CWD ?? process.cwd(),
 ) {
   const configPath = getConfigPath(cwd)
   const projectRoot = path.dirname(configPath)
+  const rootDotCedarDir = path.join(projectRoot, '.cedar')
+  const rootDotRedwoodDir = path.join(projectRoot, '.redwood')
 
-  const generatedDirPath =
-    findUp('.cedar', projectRoot) ||
-    findUp('.redwood', projectRoot) ||
-    path.join(projectRoot, '.cedar')
+  const generatedDirPath = fs.existsSync(rootDotCedarDir)
+    ? rootDotCedarDir
+    : fs.existsSync(rootDotRedwoodDir)
+      ? rootDotRedwoodDir
+      : rootDotCedarDir
 
   return generatedDirPath
 }
