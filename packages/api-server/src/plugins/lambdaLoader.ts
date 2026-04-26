4f1e8381d058d9582798c30d97d568fc9997bf59
Tobbe Lundberg <tobbe@tlundberg.com>
2026-04-26 07:34:29 +0200

    chore(api-server): Add comment belonging to #1680/#1682 (#1684)

--- packages/api-server/src/plugins/lambdaLoader.ts
+++ packages/api-server/src/plugins/lambdaLoader.ts
@@ -200,10 +200,12 @@ interface LambdaHandlerRequest extends RequestGenericInterface {
 }
 
 /**
- This will take a fastify request
- Then convert it to a lambdaEvent, and pass it to the the appropriate handler for the routeName
- The LAMBDA_FUNCTIONS lookup has been populated already by this point
- **/
+ * This will take a fastify request
+ * Then convert it to a lambdaEvent, and pass it to the the appropriate handler
+ * for the routeName
+ * The CEDAR_HANDLERS and LAMBDA_FUNCTIONS maps have been populated already by
+ * this point
+ */
 export const lambdaRequestHandler = async (
   req: FastifyRequest<LambdaHandlerRequest>,
   reply: FastifyReply,
