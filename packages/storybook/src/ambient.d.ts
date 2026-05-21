00523916aea894cfbcd9bf23744a27ab0e3a2f35
Lisa <lisa@tlundberg.com>
2026-05-21 17:04:28 +0200

    fix(storybook): prevent esbuild from pre-bundling storybook-framework-cedarjs (#1805)
    Co-authored-by: Tobbe Lundberg <tobbe@tlundberg.com>

---
+++ packages/storybook/src/ambient.d.ts
@@ -0,0 +1,6 @@
+// Virtual module alias resolved by Vite to the user's Routes file
+declare module '~__CEDAR__USER_ROUTES_FOR_MOCK' {
+  import type React from 'react'
+  const Routes: React.FC
+  export default Routes
+}
