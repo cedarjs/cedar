85dab8b00cca1444225bc289794ecb922b9dc747
Tobbe Lundberg <tobbe@tlundberg.com>
2026-05-25 10:36:26 -0700

    feat(cli): Add provider specific ud setup commands (#1818)

--- packages/cli/src/commands/setup/deploy/providers/vercel.js
+++ packages/cli/src/commands/setup/deploy/providers/vercel.js
@@ -2,4 +2,12 @@ import { createHandler } from '../helpers/helpers.js'
 
 export const command = 'vercel'
 export const description = 'Setup Vercel deploy'
+
+export const builder = (yargs) =>
+  yargs.option('ud', {
+    description: 'Setup for use with Universal Deploy',
+    type: 'boolean',
+    default: false,
+  })
+
 export const handler = createHandler('vercel')
