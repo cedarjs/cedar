b294dab756c7aebe9543e12c3a54628cf814bfcc
Tobbe Lundberg <tobbe@tlundberg.com>
2026-06-07 00:52:12 -0700

    feat(vite)!: Remove vite setup command (#1860)

--- packages/web/build.ts
+++ packages/web/build.ts
@@ -10,12 +10,8 @@ import {
 
 // CJS build
 /**
- * Notes:
- * - we don't build the webpack entry point in CJS, because it produces a double wrapped module
- * instead we use the ESM version (see ./webpackEntry in package.json). The double wrapping happens
- * when you set type: module in package.json, and occurs on the App & Routes import from the project.
- * - we build bins in CJS, until projects fully switch to ESM (or we produce .mts files) this is probably
- * the better option
+ * Note: We build bins in CJS, until projects fully switch to ESM or we produce
+ * .mts files, which is probably the better option
  */
 await build({
   entryPointOptions: {
@@ -68,8 +64,3 @@ writeFileSync('dist/cjs/package.json', JSON.stringify({ type: 'commonjs' }))
 // Place a package.json file with `type: module` in the dist folder so that
 // all .js files are treated as ES Module files.
 writeFileSync('dist/package.json', JSON.stringify({ type: 'module' }))
-
-// tsc doesn't generate any types here, because the source file is a javascript
-// file. But it's really simple. It doesn't have any exports. So we can just
-// write the type definitions ourselves
-writeFileSync('dist/entry/index.d.ts', 'export {}\n')
