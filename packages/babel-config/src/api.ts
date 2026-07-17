bfb16d6a2412fb9b6f14558f84ed9407077b1f06
Tobbe Lundberg <tobbe@tlundberg.com>
2026-07-17 10:47:49 +0200

    remove transform-runtime plugin

--- packages/babel-config/src/api.ts
+++ packages/babel-config/src/api.ts
@@ -65,9 +65,6 @@ export const getApiSideBabelPlugins = ({
     // Needed to support `/** @jsxImportSource custom-jsx-library */`
     // comments in JSX files
     !forVite && ['@babel/plugin-transform-react-jsx', { runtime: 'automatic' }],
-    // Vite/esbuild handles module transforms natively; only keep for non-Vite
-    // consumers.
-    !forVite && ['@babel/plugin-transform-runtime', {}],
     [
       'babel-plugin-module-resolver',
       {
