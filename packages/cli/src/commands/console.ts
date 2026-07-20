92aaed3e1c85de9bcabc0e07cf638b86ce5dcd35
Tobbe Lundberg <tobbe@tlundberg.com>
2026-07-20 16:19:40 +0200

    feat(cli)!: Remove `yarn cedar console` (#2137)

--- packages/cli/src/commands/console.ts
+++ packages/cli/src/commands/console.ts
@@ -1,8 +1,14 @@
 export const command = 'console'
 export const aliases = ['c']
-export const description = 'Launch an interactive Redwood shell (experimental)'
+export const description = 'Launch an interactive Cedar shell'
 
 export const handler = async () => {
-  const { handler } = await import('./consoleHandler.js')
-  return handler()
+  console.log(
+    '`cedar console` has been removed from the Cedar CLI.\n' +
+      'Run it as a standalone tool instead:\n\n' +
+      '  yarn dlx @cedarjs/console\n' +
+      '  npx @cedarjs/console\n' +
+      '  pnpm dlx @cedarjs/console\n',
+  )
+  process.exit(1)
 }
