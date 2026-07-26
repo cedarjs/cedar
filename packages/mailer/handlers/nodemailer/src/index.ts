e751d3f734b6b88b85df1bee3d43e094aa57789f
Tobbe Lundberg <tobbe@tlundberg.com>
2026-07-26 06:52:31 +0200

    fix(mailer-handler-nodemailer): Convert @cedarjs/mailer-handler-nodemailer to ESM-only (#2215)

--- packages/mailer/handlers/nodemailer/src/index.ts
+++ packages/mailer/handlers/nodemailer/src/index.ts
@@ -1,5 +1,5 @@
 import nodemailer from 'nodemailer'
-import type SMTPTransport from 'nodemailer/lib/smtp-transport'
+import type SMTPTransport from 'nodemailer/lib/smtp-transport/index.js'
 
 import { AbstractMailHandler } from '@cedarjs/mailer-core'
 import type {
